65d988d0f7fe3ffee15063d71412b3735a8c784b
Eman Aziz <aziz.eman@hotmail.com>
2022-03-29 16:44:03 -0400

    created form elements and setup functions for book info constructor and for submission event listener

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const form = document.getElementById('main-form');
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+    form.addEventListener('submit', function(e) {
+        //prevents refreshing of page after each submission
+        e.preventDefault();
+    });
+    
+}
+
+
+console.log(myLibrary);
+addBookToLibrary();
