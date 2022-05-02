53ba5d3129d4cf13b70158feb20a933b535b7f1f
Eman Aziz <aziz.eman@hotmail.com>
2022-05-01 22:35:12 -0400

    added javascript code that stores form inputs into variables and stores them into array on submission. enabled reset of form on submission.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict"
 
 const form = document.getElementById('main-form');
+const readButton = document.getElementById('read');
+const readingButton = document.getElementById('reading');
+
+
+let read = false;
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -10,13 +15,28 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+readButton.addEventListener('click', () => {
+    read = true;
+});
+
+readingButton.addEventListener('click', () => {
+    read = false;
+});
+
 function addBookToLibrary() {
     form.addEventListener('submit', function(e) {
         //prevents refreshing of page after each submission
         e.preventDefault();
+        const titleInput = document.getElementById('book-title').value;
+        const authorInput = document.getElementById('book-author').value;
+        const pagesInput = document.getElementById('book-page-count').value;
+        
+        const entry = new Book(titleInput, authorInput, pagesInput, read);
+        myLibrary.push(entry);
+        form.reset();
+        console.log(myLibrary);
     });
 }
 
-
-console.log(myLibrary);
 addBookToLibrary();
+
