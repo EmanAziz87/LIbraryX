7a69b697d2d4ae6f449977d0d0d46866f7315447
Eman Aziz <aziz.eman@hotmail.com>
2022-05-04 22:59:28 -0400

    add book button now displays form container and dims the background. added some simple text to card for a mock version.

--- script.js
+++ script.js
@@ -3,17 +3,12 @@
 const form = document.getElementById('main-form');
 const readButton = document.getElementById('read');
 const readingButton = document.getElementById('reading');
-
-
+const addBookButton = document.querySelector('.add-book');
+const formWindow = document.querySelector('.form-div-container');
+const dimPage = document.querySelector('.page-dim');
 let read = false;
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
 
 readButton.addEventListener('click', () => {
     read = true;
@@ -22,6 +17,24 @@ readButton.addEventListener('click', () => {
 readingButton.addEventListener('click', () => {
     read = false;
 });
+
+
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function closeFormWindow() {
+    formWindow.style.cssText = 'display: none;'
+}
+
+function showFormWindow() {
+    formWindow.style.cssText = 'display: flex;'
+}
+
 
 function addBookToLibrary() {
     form.addEventListener('submit', function(e) {
@@ -33,15 +46,24 @@ function addBookToLibrary() {
         const entry = new Book(titleInput, authorInput, pagesInput, read);
         myLibrary.push(entry);
         form.reset();
+        closeFormWindow();
+        dimPage.style.cssText = 'display:none;'
         console.log(myLibrary);
-
-
     });
 }
 
+addBookButton.addEventListener('click', () => {
+    showFormWindow();
+    dimPage.style.cssText = 'z-index: 3; position:absolute; width:100vw; height:100vh; background-color: rgba(0, 0, 0, 0.281);'
+});
+
 function displayCard() {
-    
+
 }
+
+
+
+
 
 addBookToLibrary();
 
