db1e17140d28dbf8a0b8ac39a92c7889ce5b4863
Eman Aziz <aziz.eman@hotmail.com>
2022-05-04 23:51:16 -0400

    added exit button on the form container.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const readingButton = document.getElementById('reading');
 const addBookButton = document.querySelector('.add-book');
 const formWindow = document.querySelector('.form-div-container');
 const dimPage = document.querySelector('.page-dim');
+const formWindowClose = document.querySelector('.close-form');
+
 let read = false;
 let myLibrary = [];
 
@@ -19,7 +21,6 @@ readingButton.addEventListener('click', () => {
 });
 
 
-
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -27,43 +28,42 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function closeFormWindow() {
-    formWindow.style.cssText = 'display: none;'
-}
-
-function showFormWindow() {
-    formWindow.style.cssText = 'display: flex;'
-}
-
-
 function addBookToLibrary() {
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         const titleInput = document.getElementById('book-title').value;
         const authorInput = document.getElementById('book-author').value;
         const pagesInput = document.getElementById('book-page-count').value;
+        resetFormAndPushToArray();
 
-        const entry = new Book(titleInput, authorInput, pagesInput, read);
-        myLibrary.push(entry);
-        form.reset();
-        closeFormWindow();
-        dimPage.style.cssText = 'display:none;'
-        console.log(myLibrary);
+        function resetFormAndPushToArray() {
+            const entry = new Book(titleInput, authorInput, pagesInput, read);
+            myLibrary.push(entry);
+            form.reset();
+            formWindow.style.cssText = 'display: none;';
+            dimPage.style.cssText = 'display:none;'
+
+            console.log(myLibrary);
+        }
     });
 }
 
+
 addBookButton.addEventListener('click', () => {
-    showFormWindow();
+    formWindow.style.cssText = 'display: flex;';
     dimPage.style.cssText = 'z-index: 3; position:absolute; width:100vw; height:100vh; background-color: rgba(0, 0, 0, 0.281);'
 });
+
+formWindowClose.addEventListener('click', () => {
+    formWindow.style.cssText = 'display: none;';
+    dimPage.style.cssText = 'display:none;'
+    form.reset();
+});
+
 
 function displayCard() {
 
 }
-
-
-
-
 
 addBookToLibrary();
 
