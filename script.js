b67ea505ab6d87024a403512631f6a74575e583a
Eman Aziz <aziz.eman@hotmail.com>
2022-05-07 21:16:38 -0400

    The project is complete. Added delete functionality that removes cards visually and from the list where book entries are stored. UX improvements with the edit button such as disabling it when all cards are deleted. Form exit now works when clicking any where except for on the form. Edit mode is locked when their are no books in your library, and adding books is prohibited when you are in edit mode. minor css styling additions as well.

--- script.js
+++ script.js
@@ -9,27 +9,84 @@ const dimPage = document.querySelector('.page-dim');
 const formWindowClose = document.querySelector('.close-form');
 const cardContainer = document.querySelector('#card-container');
 const editMode = document.querySelector('.edit-mode');
-const deleteCardButton = document.querySelector('.delete-card-button'); 
 
 
 let read = false;
 let myLibrary = [];
-
-
-readButton.addEventListener('click', () => {
-    read = true;
-});
-
-readingButton.addEventListener('click', () => {
-    read = false;
-});
-
+let editModeState = false;
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+}
+
+function addBookButtonToggle() {
+    addBookButton.addEventListener('click', () => {
+        if (!editModeState) {
+            formWindow.style.cssText = 'display: flex;';
+            dimPage.style.cssText = 'z-index: 3; position:absolute; width:100vw; height:100vh; background-color: rgba(0, 0, 0, 0.281);'
+        } else {
+            alert('You can not add books while in edit mode');
+        }    
+    }); 
+}
+
+function readButtonsForm() {
+    readButton.addEventListener('click', () => {
+        read = true;
+    });
+    
+    readingButton.addEventListener('click', () => {
+        read = false;
+    });
+}
+
+function closeFormWindow() {
+    formWindowClose.addEventListener('click', () => {
+        formWindow.style.cssText = 'display: none;';
+        dimPage.style.cssText = 'display:none;';
+        form.reset();
+    });
+
+    
+    document.addEventListener('click', (event) => {
+        if (event.target === dimPage) {
+            formWindow.style.cssText = 'display: none;';
+            dimPage.style.cssText = 'display:none;';
+            form.reset();
+        }
+    });
+}
+
+function editModeToggle() {
+    editMode.addEventListener('click', () => {
+        if (myLibrary.length > 0) {
+            editModeState = !editModeState;
+            if (editModeState) {
+                document.querySelector('aside > div > button.edit-mode').style.cssText = 'background-color: red;';
+            } else {
+                document.querySelector('aside > div > button.edit-mode').style.cssText = 'background-color:';
+            }
+            const deleteCardButton = cardContainer.querySelectorAll('.delete-card-button');
+            deleteCardButton.forEach((element) => {
+                element.style.cssText = `display: ${editModeState ? 'inline-block;' : 'none;'}`;
+                element.addEventListener('click', () => {
+                    element.parentElement.remove();
+                    myLibrary.splice(myLibrary.findIndex(function(book) {
+                        return `Title: ${book.title}` === element.nextSibling.firstChild.textContent;    
+                    }), 1);
+                    if (myLibrary.length === 0) {
+                        document.querySelector('aside > div > button.edit-mode').style.cssText = 'background-color:';
+                        editModeState = false;
+                    }
+                });
+            });
+        } else {
+            alert("You need to add books to your library to edit");
+        }
+    });
 }
 
 function bookFormSubmission() {
@@ -52,62 +109,47 @@ function bookFormSubmission() {
     });
 }
 
-
-addBookButton.addEventListener('click', () => {
-    formWindow.style.cssText = 'display: flex;';
-    dimPage.style.cssText = 'z-index: 3; position:absolute; width:100vw; height:100vh; background-color: rgba(0, 0, 0, 0.281);'
-});
-
-formWindowClose.addEventListener('click', () => {
-    formWindow.style.cssText = 'display: none;';
-    dimPage.style.cssText = 'display:none;'
-    form.reset();
-});
-
-if (myLibrary.length > 0) {
-    editMode.addEventListener('click', () => {
-        deleteCardButton.style.display = 'display: null;';
-    });
-}
-
-
-
 function displayBookCards() {
-    for (let i = myLibrary.length-1; i < myLibrary.length; i++) {
-        const newCard = document.createElement('div');
-        const newCardContents = document.createElement('div');
-        const newCardExit = document.createElement('button');
-        
-        const titleContent = document.createElement('p');
-        const authorContent = document.createElement('p');
-        const pagesContent = document.createElement('p');
-        const readOrNotContent = document.createElement('p');
+    const newCard = document.createElement('div');
+    const newCardContents = document.createElement('div');
+    const newCardExit = document.createElement('button');
 
-        newCardExit.textContent = 'X';
-        cardContainer.appendChild(newCard);
+    const titleContent = document.createElement('p');
+    const authorContent = document.createElement('p');
+    const pagesContent = document.createElement('p');
+    const readOrNotContent = document.createElement('p');
 
-        newCard.className = 'card'; 
-        newCardContents.className = 'card-subcontainer'; 
-        newCardExit.className = 'delete-card-button'; 
-        titleContent.className = 'title';
-        authorContent.className = 'author';
-        pagesContent.className = 'page-count';
-        readOrNotContent.className = 'read-or-reading';
+    newCardExit.textContent = 'X';
+    cardContainer.appendChild(newCard);
 
-        newCard.appendChild(newCardExit);
-        newCard.appendChild(newCardContents);
-        
-        newCardContents.appendChild(titleContent);
-        newCardContents.appendChild(authorContent);
-        newCardContents.appendChild(pagesContent);
-        newCardContents.appendChild(readOrNotContent);
+    newCard.className = `card`; 
+    newCardContents.className = 'card-subcontainer'; 
+    newCardExit.className = 'delete-card-button'; 
+    titleContent.className = 'title';
+    authorContent.className = 'author';
+    pagesContent.className = 'page-count';
+    readOrNotContent.className = 'read-or-reading';
 
-        titleContent.textContent = `Title: ${myLibrary[myLibrary.length - 1].title}`;
-        authorContent.textContent = `Author: ${myLibrary[myLibrary.length - 1].author}`;
-        pagesContent.textContent = `Pages: ${(myLibrary[myLibrary.length - 1].pages).toString()}`;
-        readOrNotContent.textContent = myLibrary[myLibrary.length - 1].read ? 'Read' : 'Reading';
-    }
+    newCard.appendChild(newCardExit);
+    newCard.appendChild(newCardContents);
+
+    newCardContents.appendChild(titleContent);
+    newCardContents.appendChild(authorContent);
+    newCardContents.appendChild(pagesContent);
+    newCardContents.appendChild(readOrNotContent);
+
+    titleContent.textContent = `Title: ${myLibrary[myLibrary.length - 1].title}`;
+    authorContent.textContent = `Author: ${myLibrary[myLibrary.length - 1].author}`;
+    pagesContent.textContent = `Pages: ${(myLibrary[myLibrary.length - 1].pages).toString()}`;
+    readOrNotContent.textContent = myLibrary[myLibrary.length - 1].read ? 'Read' : 'Reading';   
 }
+
+
+
+addBookButtonToggle();
+closeFormWindow();
+editModeToggle();
+readButtonsForm();
 bookFormSubmission();
        
         
